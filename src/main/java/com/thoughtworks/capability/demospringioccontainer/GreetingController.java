c3e67fb0c20c96480309005c7d696c631bf2e4ca
lemon-piepie <purplechilde@163.com>
2020-09-16 20:07:04 +0800

    finish problem2 the second way

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,23 +1,21 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private final GreetingService greetingService;
-
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        System.out.println("GreetingController is now instancing………………");
-        this.greetingService = greetingService;
+    @Lookup
+    public GreetingService greetingService(){
+        return null;
     }
 
     @GetMapping("/greet")
     public String greet() {
-        return greetingService.sayHi();
+        return greetingService().sayHi();
     }
 
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,11 +1,10 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
-@Scope(value="prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Scope(value="prototype")
 public class GreetingService {
 
     public GreetingService() {
